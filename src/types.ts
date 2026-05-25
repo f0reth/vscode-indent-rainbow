188ade6f24e98f2c451555e9904453e845726de9
f0reth <65765140+f0reth@users.noreply.github.com>
2026-05-25 23:30:17 +0900

    chore: add IndicatorStyle union type and Config interface to types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,16 @@
 import type * as vscode from "vscode";
 
+export type IndicatorStyle = "classic" | "light";
+
 export interface BuildIndentOptions {
   tabSize: number;
   colorCount: number;
   colorOnWhiteSpaceOnly: boolean;
 }
 
-export interface DecorationsOptions {
-  tabSize: number;
-  colorCount: number;
+export interface DecorationsOptions extends BuildIndentOptions {
   skipAllErrors: boolean;
   ignoreLinePatterns: RegExp[];
-  colorOnWhiteSpaceOnly: boolean;
   hasTabmix: boolean;
 }
 
@@ -19,4 +18,18 @@ export interface DecorationsResult {
   decorators: vscode.DecorationOptions[][];
   errorDecorator: vscode.DecorationOptions[];
   tabmixDecorator: vscode.DecorationOptions[];
+}
+
+export interface Config {
+  errorColor: string;
+  tabmixColor: string;
+  colorOnWhiteSpaceOnly: boolean;
+  indicatorStyle: IndicatorStyle;
+  lightIndicatorStyleLineWidth: number;
+  colors: string[];
+  ignoreLinePatterns: RegExp[];
+  updateDelay: number;
+  includedLanguages: string[];
+  excludedLanguages: string[];
+  ignoreErrorLanguages: string[];
 }
